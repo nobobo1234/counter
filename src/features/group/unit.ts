665f560f9744f5eb00424be0e149cb0016a78410
Noah van Boven <noah.van.boven@student.uva.nl>
2024-05-15 20:59:03 +0200

    Update updateUnit function to use async/await for prisma.group.update

--- src/features/group/unit.ts
+++ src/features/group/unit.ts
@@ -1,8 +1,8 @@
+"use server";
 import { prisma } from "@/db";
 
-export default function updateUnit(formData: FormData) {
-  "use server";
-  prisma.group.update({
+export default async function updateUnit(formData: FormData) {
+  await prisma.group.update({
     where: {
       id: formData.get("groupId") as string,
     },
